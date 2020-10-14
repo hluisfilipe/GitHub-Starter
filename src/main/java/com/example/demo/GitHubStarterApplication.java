bd60b29823d156c5242d28c639da9857870fa307
hluisfilipe <hluisfilipe@gmail.com>
2020-10-14 22:24:43 +0100

    A nice App

--- src/main/java/com/example/demo/GitHubStarterApplication.java
+++ src/main/java/com/example/demo/GitHubStarterApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// A really nice App
+
 @SpringBootApplication
 public class GitHubStarterApplication {
 
