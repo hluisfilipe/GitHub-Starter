c0b2a54e96f5080d1b58b897f26c7a7c17e238a4
hluisfilipe <hluisfilipe@gmail.com>
2020-10-14 22:14:44 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/GitHubStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubStarterApplication.class, args);
+	}
+
+}
